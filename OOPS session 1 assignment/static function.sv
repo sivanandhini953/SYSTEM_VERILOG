8a35dba6e13a470e3a4239d7b33cea27ecd5bb67
sivanandhini953 <sivanandhinis69@gmail.com>
2025-03-11 08:54:59 +0530

    Create static function.sv

---
+++ OOPS session 1 assignment/static function.sv
@@ -0,0 +1,28 @@
+class base ;
+static int i;
+static function void get();
+int a;
+a++;
+i++;
+$display(a);
+$display(i);
+endfunction 
+endclass
+base b1_h,b2_h;
+module test();
+initial begin
+b1_h.get();
+b1_h.get();
+b2_h.get();
+end 
+endmodule
+
+ANSWER:
+
+# run -all
+#           1
+#           1
+#           1
+#           2
+#           1
+#           3
