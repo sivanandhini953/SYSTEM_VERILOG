300893635794e4e7026462938e16e54d94d3cbcf
sivanandhini953 <sivanandhinis69@gmail.com>
2025-03-13 14:29:54 +0530

    Create Virtual class with pure virtual methods.v

---
+++ DAY 8/Virtual class with pure virtual methods.v
@@ -0,0 +1,23 @@
+virtual class BaseClass;
+	int data;
+
+	pure virtual function int getData();
+endclass
+
+class ChildClass extends BaseClass;
+	virtual function int getData();
+		data = 32'hcafe_cafe;
+		return data;
+	endfunction
+endclass
+
+module tb;
+	ChildClass child;
+	initial begin
+		child = new();
+		$display ("data = 0x%0h", child.getData());
+	end
+endmodule
+
+    ANSWER:
+     data = 0xcafecafe
